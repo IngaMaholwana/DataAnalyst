e89e306917fd900bd6398de8f8a56734ed3668ce
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-02-14 19:44:41 +0200

    load  the data

--- lib/readin.py
+++ lib/readin.py
@@ -4,6 +4,8 @@ import seaborn as sns
 
 import csv
 
-df = pd.read_csv("german_credit_data_biased_training.csv")
+# df = pd.read_csv("german_credit_data_biased_training.csv")
 
-print(df.head())
+# print(df.head())
+
+credit = pd.read_csv("german_credit_data_biased_training.csv")
